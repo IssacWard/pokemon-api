11d96043f721680cf98b21f3507976ef8be3a710
Soren <IssacSWard@yahoo.com>
2020-11-04 15:44:23 -0500

    MEssage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import PokemonCard from './Components/PokemonCard';
+import Axios from 'axios';
 
 
 function App() {
 
   const [names,setNames] =  useState([]);
+
+  useEffect(() => {
+    Axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then(res => setNames(res.data.results))
+      .catch(err => console.log(err))
+  },[]);
 
   const getPokemonNames = () => {
     fetch("https://pokeapi.co/api/v2/pokemon")
